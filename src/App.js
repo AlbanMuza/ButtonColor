390159e37658f5834fe8fc4745befca113b19421
Alban Muzanenhamo <38377066+AlbanMuza@users.noreply.github.com>
2022-08-11 00:58:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const colours = ['purple', 'silver', 'red', 'brown', 'yellow'];
+
+export function App(props) {
+    const [selectedColourIndex, setColourIndex] = useState(0);
+
+    const nextColour = () => {
+        const newColourIndex = selectedColourIndex + 1;
+        if (colours[newColourIndex]) 
+            setColourIndex(newColourIndex);
+        else
+            setColourIndex(0);
+    }
+
+    return (
+              <div className='button'>
+              <button type="button" style={{backgroundColor: colours[selectedColourIndex]}}
+            onClick={nextColour}>Change color</button>
+            </div>
+            );
 }
-
 export default App;
